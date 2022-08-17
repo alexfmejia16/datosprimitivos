05a90053e7d879386879cc06fbfab8281497053a
505 <505@academicos.cesde.edu.co>
2022-08-17 17:37:23 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,14 @@
+#hola, comentario de lina
+'''
+Hola soy un comentario de bloque pacho es un gay
+'''
+#Datos primitivos 
+
+nombre="De Pacho"
+edad=33
+estatura=1.50
+hinchaDelMejor=True
+
+#Salidas
+#print("perro: ",nombre)
+print(f'hola {nombre} tu estatura es {estatura}')
